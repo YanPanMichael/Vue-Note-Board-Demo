8dc8f4d07c5bf453e6ef1f66cf9ddf7b0e0aded6
YanPanMichael <yanpancsu@icloud.com>
2018-06-13 00:59:46 +0800

    try to seperate as components

---
+++ index.vue
@@ -0,0 +1,11 @@
+<template id="mainHead">
+    <div class="page-top">
+        <div class="page-content wrap">
+            <h2>计划任务表</h2>
+        </div>
+    </div>
+</template>
+
+Vue.component('mainHead', {
+    template: '#mainHead'
+})
