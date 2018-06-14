f7ddbea58fc1716aaa8b152c0ff24acb837a10d0
YanPanMichael <yanpancsu@icloud.com>
2018-06-15 01:11:50 +0800

    save

--- index.vue
+++ index.vue
@@ -1,11 +1,67 @@
-<template id="mainHead">
-    <div class="page-top">
-        <div class="page-content wrap">
-            <h2>计划任务表</h2>
-        </div>
+<style>
+    * {
+        margin: 0;
+        padding: 0
+    }
+
+    li {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+    }
+
+    img {
+        border: none;
+        display: block;
+    }
+
+    .clearFix:after {
+        content: '';
+        display: block;
+        clear: both;
+        height: 0;
+        width: 0;
+    }
+
+    .clearFix {
+        zoom: 1
+    }
+
+    .fl {
+        float: left;
+    }
+
+    .fr {
+        float: right;
+    }
+
+    .wrap {
+        width: 980px;
+        margin: 0 auto;
+    }
+</style>
+
+<template>
+    <div id="main">
+        <MainHeader></MainHeader>
+        <MainWrapper></MainWrapper>
     </div>
 </template>
 
-Vue.component('mainHead', {
-    template: '#mainHead'
-})
+<script>
+    import MainHeader from './main-header.vue'
+    import MainWrapper from './main-wrapper.vue'
+    new Vue({
+        //这里面是选项对象，里面有很多值
+        el: "#main",//挂在点，挂在到main上
+        data: {
+        },
+        components: {
+            MainHeader,
+            MainWrapper
+        }
+    });
+</script>
---
+++ main-header.vue
@@ -0,0 +1,32 @@
+<style>
+    .page-top {
+        background-color: #e8593c;
+        height: 40px;
+        border-top: 1px solid #b56553;
+        border-bottom: 1px solid #b56553;
+    }
+
+    .page-top .page-content h2 {
+        width: 100%;
+        line-height: 40px;
+        color: #fff;
+        text-align: center;
+        font-size: 22px;
+    }
+</style>
+<template id="mainHead">
+    <div class="page-top">
+        <div class="page-content wrap">
+            <h2>计划任务表</h2>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'main-header',
+    template: '#mainHead',
+    data () {
+        return { }
+    }
+}
+</script>
