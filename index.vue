9f931c53377629a1cac5f44e9fe87c8b38774db3
YanPanMichael <yanpancsu@icloud.com>
2018-06-19 23:53:19 +0800

    save

--- index.vue
+++ index.vue
@@ -1,49 +1,3 @@
-<style>
-    * {
-        margin: 0;
-        padding: 0
-    }
-
-    li {
-        list-style: none;
-    }
-
-    a {
-        text-decoration: none;
-        color: #333;
-    }
-
-    img {
-        border: none;
-        display: block;
-    }
-
-    .clearFix:after {
-        content: '';
-        display: block;
-        clear: both;
-        height: 0;
-        width: 0;
-    }
-
-    .clearFix {
-        zoom: 1
-    }
-
-    .fl {
-        float: left;
-    }
-
-    .fr {
-        float: right;
-    }
-
-    .wrap {
-        width: 980px;
-        margin: 0 auto;
-    }
-</style>
-
 <template>
     <div id="main">
         <MainHeader></MainHeader>
@@ -52,13 +6,12 @@
 </template>
 
 <script>
+    import './index.css'
     import MainHeader from './main-header.vue'
     import MainWrapper from './main-wrapper.vue'
-    new Vue({
+    var vueObj = new Vue({
         //这里面是选项对象，里面有很多值
         el: "#main",//挂在点，挂在到main上
-        data: {
-        },
         components: {
             MainHeader,
             MainWrapper
